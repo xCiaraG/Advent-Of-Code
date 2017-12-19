c422fbc64e4dd4e7afa296e373411bc46d135bd6
xCiaraG <ciara.godwin3@mail.dcu.ie>
2017-12-19 16:03:15 +0000

    Add day 19

---
+++ 2017/day19_puzzle02.py
@@ -0,0 +1,43 @@
+from sys import stdin
+
+def in_bounds(x, y):
+   return 0 <= x < len(m) and 0 <= y < len(m[0])
+
+lines = stdin.readlines()
+m = []
+for line in lines:
+   m.append(list(line)[:-1])
+
+x, y = 0, m[0].index("|")
+tmpx, tmpy = x, y
+count = 0
+seen = set([(x, y)])
+nsew = {"n" : (-1, 0), "s" : (1, 0), "e" : (0, 1), "w" : (0, -1)}
+d = "s"
+while True:
+   while in_bounds(tmpx, tmpy) and m[tmpx][tmpy].strip():
+      count += 1
+      x = tmpx
+      y = tmpy
+      seen.add((x, y))
+      tmpx += nsew[d][0]
+      tmpy += nsew[d][1]
+   if in_bounds(x - 1, y) and (x - 1, y) not in seen and m[x - 1][y].strip():
+      tmpx = x - 1
+      tmpy = y
+      d = "n"
+   elif in_bounds(x + 1, y) and (x + 1, y) not in seen and m[x + 1][y].strip():
+      tmpx = x + 1
+      tmpy = y
+      d = "s"
+   elif in_bounds(x, y - 1) and (x, y - 1) not in seen and m[x][y - 1].strip():
+      tmpx = x 
+      tmpy = y - 1
+      d = "w"
+   elif in_bounds(x, y + 1) and (x, y + 1) not in seen and m[x][y + 1].strip():
+      tmpx = x
+      tmpy = y + 1
+      d = "e"
+   else: 
+      break
+print(count)
